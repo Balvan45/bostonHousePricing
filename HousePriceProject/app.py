354a2fda4ff94e8e03962a68ad954c1b8d9f93bf
Balvan Kumar <ashwinbalvan2004@gmail.com>
2026-05-06 18:27:00 +0530

    The app is ready

--- HousePriceProject/app.py
+++ HousePriceProject/app.py
@@ -4,8 +4,10 @@ import numpy as np
 import pandas as pd
 import os
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
-app=Flask(__name__)#starting point of application where the application starts 
+app=Flask(__name__,template_folder=os.path.join(base_dir, 'templates'),
+            static_folder=os.path.join(base_dir, 'static'))#starting point of application where the application starts 
 model=pickle.load(open('regmodel.pkl','rb'))#Load the model
 scaler =pickle.load(open('scaling.pkl','rb'))
 
@@ -15,19 +17,11 @@ def check():
 
 @app.route('/')# Go to my home the '/' Should be says like that
 def home():
-    return """<!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-</head>
-<body>
-    <h1>Flask is working 🚀</h1>
-</body>
-</html>"""
+    return render_template("home.html")
 
 @app.route('/predict_api',methods=['POST'])#capture the input and send to the model and model give the output
 def predict_api():
-    data=request.json['data']#when click the 'predict_api' then it request through the json and capture and stored in the dat variable
+    data=request.json['data']#when click the 'predict_api' then it request through the json and capture and stored in the data variable
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     new_transformData=scaler.transform(np.array(list(data.values())).reshape(1,-1))
@@ -35,5 +29,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]#Capture the values when we fill the values in the form 
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The predicted House price is{} ".format(output))
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8080)
